3bb5729bf742a4ea34a56862d80a1b68f4c6741c
uop966 <prestige.hxs@gmail.com>
2017-07-10 12:31:50 -0400

    added display for the cost of options and cps

--- index.js
+++ index.js
@@ -5,30 +5,31 @@ var amountPerClick = 1;
 
 var onClick = function() {
     clicks += amountPerClick;
-    console.log("document.getElementById")
-    document.getElementById("click_count").innerHTML = "Clicks: " + clicks;
+    render();
 }
 
 const block1CPS = 1;
 const block2CPS = 5;
 const block1Cost = 10;
 const block2Cost = 50;
-var block1s = 0;
-var block2s = 0;
+var block1Count = 0;
+var block2Count = 0;
 
 function click1() {
   if (clicks >= block1Cost) {
     clicks = clicks - block1Cost;
-    block1s++;
+    block1Count++;
     clicksPerSecond += block1CPS;
+    render();
   }
 }
 
 function click2() {
   if (clicks >= block2Cost) {
     clicks = clicks - block2Cost;
-    block2s++;
+    block2Count++;
     clicksPerSecond += block2CPS;
+    render();
   }
 }
 
@@ -36,5 +37,17 @@ var gainz = setInterval(updateClicks, 1000);
 
 function updateClicks() {
   clicks += clicksPerSecond;
+  render();
+}
+
+function render() {
   document.getElementById("click_count").innerHTML = "Clicks: " + clicks;
+  document.getElementById("cps1").innerHTML = block1CPS;
+  document.getElementById("cost1").innerHTML = block1Cost;
+  document.getElementById("count1").innerHTML = block1Count;
+
+  document.getElementById("cps2").innerHTML = block2CPS;
+  document.getElementById("cost2").innerHTML = block2Cost;
+  document.getElementById("count2").innerHTML = block2Count;
+
 }
